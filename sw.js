e41f1c44680de2c48afe843135917722dc912d8f
deftwebmaster <deftwebmaster@users.noreply.github.com>
2023-02-28 14:55:02 -0600

    ...

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230228.144938';
+const cacheName = 'chirpy-20230228.145423';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
